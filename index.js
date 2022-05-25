d88bda99bf1860021b3e168558457e86964234fe
chochozabe <cintamani_whyk@naver.com>
2022-05-26 01:55:47 +0900

    feat : localStorge에 todo 저장

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const todoList = document.querySelector(".todo-list");
 let todos = [];
 let id = 0;
 
-const setTodos = (newTodos) => {
-  todos = newTodos;
-};
-
-const getTodos = () => {
-  return todos;
+const setTodos = () => {
+  localStorage.setItem("TODO_LIST", JSON.stringify(todos));
 };
 
 const addTodo = (newTodo) => {
@@ -21,7 +17,7 @@ const addTodo = (newTodo) => {
     content: newTodo,
   };
 
-  setTodos([...todos, tmpTodo]);
+  todos = [...todos, tmpTodo];
 
   paintTodos();
 };
@@ -29,49 +25,53 @@ const addTodo = (newTodo) => {
 const paintTodos = () => {
   todoList.innerHTML = "";
 
-  todos.forEach((todo) => {
-    const todoItemElem = document.createElement("li");
-    todoItemElem.className = "todo-item";
+  todos
+    ? todos.forEach((todo) => {
+        const todoItemElem = document.createElement("li");
+        todoItemElem.className = "todo-item";
 
-    const checkboxElem = document.createElement("div");
-    checkboxElem.className = "checkbox";
-    checkboxElem.addEventListener("click", () => {
-      checkTodo(todo.id);
-    });
+        const checkboxElem = document.createElement("div");
+        checkboxElem.className = "checkbox";
+        checkboxElem.addEventListener("click", () => {
+          checkTodo(todo.id);
+        });
 
-    const todoElem = document.createElement("div");
-    todoElem.className = "todo";
-    todoElem.innerText = todo.content;
+        const todoElem = document.createElement("div");
+        todoElem.className = "todo";
+        todoElem.innerText = todo.content;
 
-    const delBtnElem = document.createElement("button");
-    delBtnElem.className = "delBtn";
-    delBtnElem.innerHTML = "X";
-    delBtnElem.addEventListener("click", () => {
-      deleteTodo(todo.id);
-    });
+        const delBtnElem = document.createElement("button");
+        delBtnElem.className = "delBtn";
+        delBtnElem.innerHTML = "X";
+        delBtnElem.addEventListener("click", () => {
+          deleteTodo(todo.id);
+        });
 
-    if (todo.isComplete) {
-      todoItemElem.classList.add("checked");
-      checkboxElem.innerText = "✔";
-    }
+        if (todo.isComplete) {
+          todoItemElem.classList.add("checked");
+          checkboxElem.innerText = "✔";
+        }
 
-    todoItemElem.appendChild(checkboxElem);
-    todoItemElem.appendChild(todoElem);
-    todoItemElem.appendChild(delBtnElem);
+        todoItemElem.appendChild(checkboxElem);
+        todoItemElem.appendChild(todoElem);
+        todoItemElem.appendChild(delBtnElem);
 
-    todoList.appendChild(todoItemElem);
-  });
+        todoList.appendChild(todoItemElem);
+      })
+    : "";
+
+  setTodos();
 };
 
 const deleteTodo = (todoId) => {
-  const newTodos = getTodos().filter((todo) => todo.id !== todoId);
+  const newTodos = todos.filter((todo) => todo.id !== todoId);
 
-  setTodos(newTodos);
+  todos = newTodos;
   paintTodos();
 };
 
 const checkTodo = (todoId) => {
-  const newTodos = getTodos().map((todo) => {
+  const newTodos = todos.map((todo) => {
     if (todo.id === todoId) {
       return {
         ...todo,
@@ -82,11 +82,22 @@ const checkTodo = (todoId) => {
     return todo;
   });
 
-  setTodos(newTodos);
+  todos = newTodos;
   paintTodos();
 };
 
+const getTodos = () => {
+  const storageTodos = localStorage.getItem("TODO_LIST");
+
+  if (storageTodos) {
+    todos = JSON.parse(storageTodos);
+    paintTodos();
+  }
+};
+
 const init = () => {
+  getTodos();
+
   todoInput.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
       addTodo(e.target.value);
